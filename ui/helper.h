cc9b99fa88fcf53cb18a841f79c03edf7da195ee
Slymaster <sylvain.rey.pro@gmail.com>
2019-04-29 11:54:40 +0200

    helper.h

---
+++ ui/helper.h
@@ -0,0 +1,15 @@
+#ifndef _HELPER_H_
+#define _HELPER_H_
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+#define WINDOW_WIDTH (800)
+#define WINDOW_HEIGHT (600)
+
+// speed in pixels/second
+#define SPEED (120)
+
+#endif
