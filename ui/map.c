99f5764004f3eb51219876377d3a50763c27ef81
Sylvain <sylvain.rey.pro@gmail.com>
2019-05-06 17:48:03 +0200

    Update map.c

--- ui/map.c
+++ ui/map.c
@@ -1,23 +1,4 @@
-/**
- * hello6_keyboard.c - Move the sprite using the arrow keys or WASD
- */
-
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_image.h>
-
-#define WINDOW_WIDTH (600)
-#define WINDOW_HEIGHT (600)
-
-#define TILE_WIDTH 60
-#define TILE_HEIGHT 60
-
-#define NUMBER_BLOCS_WIDTH 10
-#define NUMBER_BLOCS_HEIGHT 10
-
-// speed in pixels/second
-#define SPEED (300)
+#include "helper.h"
 
 int map(void)
 {
