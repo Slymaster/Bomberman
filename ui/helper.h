f7dbdb4849491983f8132360e8638ad080ca72ae
Sylvain <sylvain.rey.pro@gmail.com>
2019-05-06 17:48:42 +0200

    Update helper.h in relation to map.c

--- ui/helper.h
+++ ui/helper.h
@@ -1,19 +1,22 @@
 #ifndef _HELPER_H_
 #define _HELPER_H_
 
-#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
-#include <math.h>
-#include <stdlib.h>
+#include <SDL2/SDL_image.h>
 
-#define WINDOW_WIDTH (992)
-#define WINDOW_HEIGHT (416)
+#define WINDOW_WIDTH (600)
+#define WINDOW_HEIGHT (600)
+
+#define TILE_WIDTH 60
+#define TILE_HEIGHT 60
+
+#define NUMBER_BLOCS_WIDTH 10
+#define NUMBER_BLOCS_HEIGHT 10
 
 // speed in pixels/second
 #define SPEED (300)
-#define TILE_WIDTH (32)
 
 int map();
 
